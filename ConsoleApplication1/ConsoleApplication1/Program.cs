9649aefbec731e03374a5176e41af45c1d59d0e2
Alan Yu <callyuhuanquan@live.com>
2016-11-04 21:51:47 +0800

    for the hotfix

--- ConsoleApplication1/ConsoleApplication1/Program.cs
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -10,6 +10,8 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("code for hotfix 1");
+            Console.WriteLine("code for hotfix 2");
         }
     }
 }
